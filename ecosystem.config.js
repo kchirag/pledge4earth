54099cc6d4b2fad84f2e8959143ab71074a89c96
Chris <kchirag@gmail.com>
2023-04-16 15:16:42 -0700

    sendgrid ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,8 +13,8 @@ module.exports = {
 	PRIVATE_KEY:'/usr/local/ssl/lead4earth.org.key',
 	CERTIFICATE:'/usr/local/ssl/lead4earth_org.crt',
 	CERTIFICATECA:'/usr/local/ssl/lead4earth_org.ca-bundle',
-  STORAGEKEY="../amiable-octane-375722-a20e65eb5c57.json"
-  SENDGRIDKEY="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"
+  STORAGEKEY="../amiable-octane-375722-a20e65eb5c57.json",
+  SENDGRIDKEY="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",
 	      
       },
     },
