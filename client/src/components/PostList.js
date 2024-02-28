5b555b31184ef3f6fd1fd8b53472c2e4d1455b5c
Chirag Kathrani <kchirag@gmail.com>
2024-02-27 17:48:27 -0800

    added source url for reputation management

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -4,11 +4,35 @@ const PostList = ({ posts }) => {
     if (!posts.length) {
         return <div>No posts available</div>;
     }
+    const source = (mediaUrl) => {
+        if (!mediaUrl) {
+            return null;
+        }
+
+        if (mediaUrl.endsWith('.mp4')) {
+            return <video controls src={mediaUrl} />;
+        } else if (mediaUrl.includes('twitter.com')) {
+            // Embed Twitter content
+            // Actual embedding would require using Twitter's widgets.js
+            return <a href={mediaUrl}>View Tweet</a>;
+        } else if (mediaUrl.includes('facebook.com')) {
+            // Embed Facebook content
+            // Actual embedding would require using Facebook's JavaScript SDK
+            return <a href={mediaUrl}>View on Facebook</a>;
+        } else if (mediaUrl.includes('instagram.com')) {
+            // Embed Instagram content
+            // Actual embedding would require using Facebook's JavaScript SDK for Instagram
+            return <a href={mediaUrl}>View on Instagram</a>;
+        } else {
+            return <img src={mediaUrl} alt="Post media" />;
+        }
+    };
 
     return (
         <div>
             {posts.map(post => (
                 <div key={post._id} className="post">
+                    <p>{source(post.url)}</p>
                     <p>{post.text}</p>
                     {post.media && post.media.map((mediaUrl, index) => (
                         // Assuming mediaUrl is the path to the media
