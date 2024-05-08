2b92e0b10dfe7e62d5f26843625e5ddb24afb50b
Chirag Kathrani <kchirag@gmail.com>
2024-05-08 13:17:32 -0700

    Fixing the main video

--- client/src/components/NewsContainer.js
+++ client/src/components/NewsContainer.js
@@ -6,8 +6,8 @@ const NewsContainer = () => {
       
       {/* Static data to be changed later */}
       
-      <iframe width="600" height="400" src="https://www.youtube.com/watch?v=A6PlwoBcV7g" title="Lead4Earth Vision and Motivation" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <h6>Narrated by Glory Rose Roy</h6>
+      <iframe width="600" height="400" src="https://www.youtube.com/embed/A6PlwoBcV7g?si=mtF9MIdE9L1mu-tz" title="Lead4Earth Vision and Motivation" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <h6>Narrated by <a Glory Rose Roy</h6>
     </div>
   );
 
